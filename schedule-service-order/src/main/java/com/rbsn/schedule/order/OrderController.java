d4f7bd846dddef06d2cb2e1025db42c64f7c684a
gaop <ynigapn@gmail.com>
2019-04-25 17:37:12 +0800

    修改config配置

--- schedule-service-order/src/main/java/com/rbsn/schedule/order/OrderController.java
+++ schedule-service-order/src/main/java/com/rbsn/schedule/order/OrderController.java
@@ -2,25 +2,22 @@ package com.rbsn.schedule.order;
 
 import com.alibaba.fastjson.JSONObject;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Configuration
+
 @RestController
-@EnableAutoConfiguration
 @RefreshScope
 public class OrderController {
 
     @Value("${test}")
-    private String url;
+    private String name;
 
     @GetMapping("call")
     public JSONObject call(){
         JSONObject jsonObject = new JSONObject();
-        jsonObject.put("url",url);
+        jsonObject.put("url",name);
         return jsonObject;
     }
 }
